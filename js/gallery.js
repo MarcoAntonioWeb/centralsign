cadb12144c972eb57f493ee8f9fe12d77506193b
seu-usersaneme <seu-email>
2021-06-16 22:04:36 -0300

    atualização

--- js/gallery.js
+++ js/gallery.js
@@ -1,7 +1,11 @@
-let imagens = document.querySelectorAll('.zoom'); // variavel recebe as imagens Geral.
-let modalGeral = document.querySelector('.modalGeral'); //variavel recebe configuração class.
-let modalImg = document.querySelector('#modal_img'); // variavel recebe imagens Modal.
-let btClose = document.querySelector('#bt_close'); // variavel recebe o botão.
+let imagens = document.querySelectorAll('.zoom'); 
+// variavel recebe as imagens Geral.
+let modalGeral = document.querySelector('.modalGeral'); 
+//variavel recebe configuração class.
+let modalImg = document.querySelector('#modal_img'); 
+// variavel recebe imagens Modal.
+let btClose = document.querySelector('#bt_close'); 
+// variavel recebe o botão.
 let srcVal = "";
 
 for(let i = 0; i < imagens.length; i++) {
@@ -10,13 +14,15 @@ for(let i = 0; i < imagens.length; i++) {
 
         srcVal = imagens[i].getAttribute('src');
         modalImg.setAttribute('src', srcVal);
-        modalGeral.classList.toggle('modal_active');//(toggle) função de ativar ou desativar a class
+        modalGeral.classList.toggle('modal_active');
+        //(toggle) função de ativar ou desativar a class
     });
 }
 
 
 /****** Criar o evento para desativar o botão  *******/
 btClose.addEventListener('click', function() {
-    modalGeral.classList.toggle('modal_active') //(toggle) função ativa e desativa o botão da class
+    modalGeral.classList.toggle('modal_active') 
+    //(toggle) função ativa e desativa o botão da class
 })
   
